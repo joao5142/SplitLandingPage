28c0f4792713bcd2a7a82a29750aec06f6233b37
joaop5142 <joaopaulo3687@hotmail.com>
2021-12-29 16:16:01 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const container = document.querySelector('.container');
+const left = document.querySelector('.split.left');
+const right = document.querySelector('.split.right');
+const links = document.querySelectorAll('.split a');
+
+links.forEach(link => {
+    link.innerHTML = link.innerText.split('').map((letter, idx) => {
+        let i = ' ';
+        if (idx == 3) {
+            return `<span style="margin-left:5px;transition-delay:${idx*50}ms">${letter}</span>`
+        }
+        return `<span style="transition-delay:${idx*50}ms">${letter}</span>`
+    }).join('');
+})
+
+left.addEventListener('mouseenter', () => {
+    container.classList.add('other-width');
+});
+left.addEventListener('mouseleave', () => {
+    container.classList.remove('other-width');
+});
+
+right.addEventListener('mouseenter', () => {
+    container.classList.add('width-change');
+});
+right.addEventListener('mouseleave', () => {
+    container.classList.remove('width-change');
+});
